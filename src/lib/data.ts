05730dfac27d754da2786afd7d0557273d0f8411
Dianella Sy <dianellabsy@csu.fullerton.edu>
2026-02-26 10:29:48 -0800

    Added Midterm Questions for Spring 2026 Semester

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -13,47 +13,44 @@ export interface Question {
 export const questions: Question[] = [
   {
     id: 1,
-    prompt: "What are some topics that will be on the midterm?",
+    prompt: "What are the topics that will be on the midterm?",
     answers: [
-      { text: "IEEE", points: 35, revealed: false },
-      { text: "GDB", points: 25, revealed: false },
-      { text: "Macros", points: 20, revealed: false },
-      { text: "Why and when to use 'C' in some prototype", points: 10, revealed: false },
-      { text: "Array of ints and floats passed to a function using GPRS and not xmmm registers", points: 6, revealed: false },
-      { text: "Kings & Queens", points: 4, revealed: false }
+      { text: "Script files written in bash language", points: 4, revealed: false },
+      { text: "General registers", points: 4, revealed: false },
+      { text: "Licenses for intellectual property", points: 4, revealed: false },
+      { text: "Hardware", points: 4, revealed: false },
+      { text: "Programming", points: 4, revealed: false },
+      { text: "Macros", points: 4, revealed: false },
+      { text: "System function (chapter 13)", points: 4, revealed: false },
+      { text: "Calling functions written in assembly", points: 4, revealed: false },
+      { text: "Heroes of computer science", points: 4, revealed: false },
+      { text: "Unsigned integers", points: 4, revealed: false },
+      { text: "Twos complement", points: 4, revealed: false },
+      { text: "IEEE754", points: 4, revealed: false },
+      { text: "Architecture", points: 4, revealed: false },
+      { text: "Endian", points: 4, revealed: false },
+      { text: "X86 Arithmetic Instructions", points: 4, revealed: false },
+      { text: "X86 Other Instructions", points: 4, revealed: false },
+      { text: "Library functions", points: 4, revealed: false },
+      { text: "Make a function written in assembly", points: 4, revealed: false },
+      { text: "Tool chain", points: 4, revealed: false },
+      { text: "Widen a general purpose register", points: 4, revealed: false },
+      { text: "Widen a subregister of rax and stay within rax", points: 4, revealed: false },
+      { text: "Widen a subregister of rax and use rdx for the additional storage", points: 4, revealed: false },
+      { text: "Compare two values", points: 4, revealed: false },
+      { text: "A list of all the possible jumps for cmp and another list of jumps for ucomisd", points: 4, revealed: false },
+      { text: "How to make a loop", points: 4, revealed: false },
+      { text: "Know the meanings of push and pop", points: 4, revealed: false }
     ]
   },
   {
     id: 2,
     prompt: "What can a student do to better prepare himself/herself before the CPSC 240 midterm?",
     answers: [
-      { text: "Study the GDB lecture notes", points: 40, revealed: false },
-      { text: "Eat breakfast", points: 30, revealed: false },
-      { text: "Study with your classmates", points: 15, revealed: false },
-      { text: "Volunteer to try to debug Assignment 3 written by a struggling classmate", points: 8, revealed: false },
-      { text: "Set up your exam environment before the exam", points: 5, revealed: false },
-      { text: "Create a draft of your email", points: 2, revealed: false }
-    ]
-  },
-  {
-    id: 3,
-    prompt: "What are some potential questions that will show up on the CPSC 240 concept midterm?",
-    answers: [
-      { text: "Kings and queens", points: 95, revealed: false },
-      { text: "Distance between a decimal and its next highest neighbor", points: 90, revealed: false },
-      { text: "IEEE754 (normal & subnormal)", points: 85, revealed: false },
-      { text: "GDB", points: 80, revealed: false },
-      { text: "LSB, MSB, Big/Little Endian", points: 75, revealed: false },
-      { text: "If a register has only # bits, what is the largest two's complement signed/unsigned register possible?", points: 70, revealed: false },
-      { text: "How do you find positive infinity in IEEE and base 10 form?", points: 65, revealed: false },
-      { text: "What is the smallest normal number?", points: 60, revealed: false },
-      { text: "Chapters 1-2: Why should we use assembly programming?", points: 55, revealed: false },
-      { text: "Licenses", points: 50, revealed: false },
-      { text: "What is activation record (AR)? What is its purpose and where is it?", points: 45, revealed: false },
-      { text: "Arrays: What is the block of assembly that wil add all the numbers stored in r15?", points: 40, revealed: false },
-      { text: "How do you remove the newline character that was recently used by fgets?", points: 35, revealed: false },
-      { text: "How do you make a block that outputs two floats in one call to printf?", points: 30, revealed: false },
-      { text: "How do you swap the value in registers r8 and r9?", points: 25, revealed: false }
+      { text: "Study the lecture notes", points: 25, revealed: false },
+      { text: "Eat breakfast", points: 25, revealed: false },
+      { text: "Study with your classmates", points: 25, revealed: false },
+      { text: "Volunteer to try to debug Assignment 3 written by a struggling classmate", points: 25, revealed: false }
     ]
   }
 ];
